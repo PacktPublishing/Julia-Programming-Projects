aba8a230d355df2ee946ba9d9724ee9e19f5806b
Adrian Salceanu <e@essenciary.com>
2018-10-26 22:09:22 +0200

    Chapter 6

--- Chapter 6/item_based_recommendations.jl
+++ Chapter 6/item_based_recommendations.jl
@@ -1,4 +1,4 @@
-using CSV, DataFrames
+using CSV, DataFrames, DelimitedFiles, Statistics
 
 const minimum_similarity = 0.8
 
@@ -6,7 +6,9 @@ function setup_data()
   movies = readdlm("top_10_movies_user_rankings.tsv", '\t')
   movies = permutedims(movies, (2,1))
   movies = convert(DataFrame, movies)
-  names = Array(movies[1, :])[1,:]
+
+  names = convert(Array, movies[1, :])[1,:]
+
   names!(movies, [Symbol(name) for name in names])
   deleterows!(movies, 1)
   rename!(movies, [Symbol("Movie title") => :User])
@@ -19,10 +21,10 @@ function movie_similarity(target_movie)
     movie == target_movie && continue
     ratings = movies[:, [movie, target_movie]]
 
-    common_users = ratings[Array(ratings[movie] .>= 0) .& Array(ratings[target_movie] .> 0), :]
+    common_users = ratings[(ratings[movie] .>= 0) .& (ratings[target_movie] .> 0), :]
 
     correlation = try
-      cor(Array(common_users[movie]), Array(common_users[target_movie]))
+      cor(common_users[movie], common_users[target_movie])
     catch
       0.0
     end
@@ -36,19 +38,26 @@ end
 
 function recommendations(target_movie)
   recommended = Dict{String,Vector{Tuple{String,Float64}}}()
+
+  # @show target_movie
+  # @show movie_similarity(target_movie)
+
   for (movie, similarity) in movie_similarity(target_movie)
     movie == target_movie && continue
     similarity > minimum_similarity || continue
 
     # println("Checking to which users we can recommend $movie")
+
     recommended["$movie"] = Vector{Tuple{String,Float64}}()
 
     for user_row in eachrow(movies)
       if user_row[target_movie] >= 5
         # println("$(user_row[:User]) has watched $target_movie so we can recommend similar movies")
+
         if user_row[movie] == 0
           # println("$(user_row[:User]) has not watched $movie so we can recommend it")
           # println("Recommending $(user_row[:User]) the movie $movie")
+
           push!(recommended["$movie"], (user_row[:User], user_row[target_movie] * similarity))
         end
       end
--- Chapter 6/user_based_movie_recommendations.jl
+++ Chapter 6/user_based_movie_recommendations.jl
@@ -1,4 +1,4 @@
-using CSV, DataFrames
+using CSV, DataFrames, Statistics
 
 const minimum_similarity = 0.8
 const movies = CSV.read("top_10_movies_user_rankings.tsv", delim = '\t')
@@ -8,19 +8,20 @@ function user_similarity(target_user)
 
   for user in names(movies[:, 2:end])
     user == target_user && continue
+
     ratings = movies[:, [user, target_user]]
-    common_movies = ratings[Array(ratings[user] .>= 0) .& Array(ratings[target_user] .> 0), :]
+    common_movies = ratings[(ratings[user] .> 0) .& (ratings[target_user] .> 0), :]
+    # common_movies = ratings[(ratings[user] .> 7) .& (ratings[target_user] .> 0), :]
 
     correlation = try
-      cor(Array(common_movies[user]), Array(common_movies[target_user]))
+      cor(common_movies[user], common_movies[target_user])
     catch
       0.0
     end
 
-    similarity[user] = correlation # Dict(:Comey=>1.0,:Dean=>-0.907841,:Missie=>NaN,:Kit=>0.774597,:Musk=>0.797512,:Sam=>0.0,:Acton=>0.632456)
+    similarity[user] = correlation
   end
 
-  # println("$target_user $similarity")
   similarity
 end
 
@@ -31,16 +32,16 @@ function recommendations(target_user)
     similarity > minimum_similarity || continue
 
     ratings = movies[:, [Symbol("Movie title"), user, target_user]]
-    recommended_movies = ratings[Array(ratings[user] .>= 7) .& Array(ratings[target_user] .== 0), :]
+    recommended_movies = ratings[(ratings[user] .>= 7) .& (ratings[target_user] .== 0), :]
 
     for movie in eachrow(recommended_movies)
-      recommended[get(movie[Symbol("Movie title")])] = get(movie[user]) * similarity
+      recommended[movie[Symbol("Movie title")]] = movie[user] * similarity
     end
   end
 
   recommended
 end
 
-for user in ["Acton", "Annie", "Comey", "Dean", "Kit", "Missie", "Musk", "Sam"]
-  println("Recommendations for $user: $(recommendations(Symbol(user)))")
+for user in names(movies)[2:end]
+  println("Recommendations for $user: $(recommendations(user))")
 end
